c2c5bc878c814836faed602ed99477b38a9a56f0
Yang Xia <55853655+xiazcy@users.noreply.github.com>
2021-07-21 13:39:21 -0700

    Code clean up for PR

--- src/main/java/org/twilmes/sql/gremlin/processor/SingleQueryExecutor.java
+++ src/main/java/org/twilmes/sql/gremlin/processor/SingleQueryExecutor.java
@@ -77,13 +77,9 @@ public class SingleQueryExecutor {
     private static final String ID_KEY = "__id__";
     private static final long RESULT_LIMIT = 10000;
     private static final int DEFAULT_PAGE_SIZE = 1000;
-    // default to 1000
     private static int pageSize = DEFAULT_PAGE_SIZE;
-    // calcite relational expression
     private final RelNode node;
-    // defines table format
     private final TableDef table;
-    // gremlin traversal
     private GraphTraversal<?, ?> traversal;
     private SqlGremlinQueryResult sqlGremlinQueryResult;
 
@@ -96,7 +92,6 @@ public class SingleQueryExecutor {
     public List<Object> run() {
         final List<Object> rowResults;
         if (!isConvertable(node)) {
-            // go until we hit a converter to find the input
             RelNode input = node;
             RelNode parent = node;
             while (!((input = input.getInput(0)) instanceof GremlinToEnumerableConverter)) {
@@ -108,19 +103,13 @@ public class SingleQueryExecutor {
             final List<Object> results = traversal.as("table_0").select("table_0").toList();
             final List<Object> rows = new ArrayList<>();
 
-            // go through each result
             for (final Object o : results) {
-                // gremlin result object
                 final Element res = (Element) o;
-                // a list of objects
                 final Object[] row = new Object[fieldNames.size()];
                 int colNum = 0;
-                // for all the fields
                 for (final String field : fieldNames) {
-                    // get property name
                     final String propName = TableUtil.getProperty(table, field);
                     final int keyIndex = propName.toLowerCase().indexOf("_id");
-                    // get value
                     Object val = null;
                     if (keyIndex > 0) {
                         // is it a pk or fk?
@@ -134,19 +123,15 @@ public class SingleQueryExecutor {
                         val = res.property(propName).value();
                         val = TableUtil.convertType(val, table.getColumn(field));
                     }
-                    // put value into row
                     row[colNum] = val;
                     colNum++;
                 }
-                // add row into result row list
                 rows.add(row);
             }
 
-            // create a traversal scan object
             final GremlinTraversalScan traversalScan =
                     new GremlinTraversalScan(input.getCluster(), input.getTraitSet(), rowType, rows);
 
-            // converts traversal to rel
             final GremlinTraversalToEnumerableRelConverter converter =
                     new GremlinTraversalToEnumerableRelConverter(input.getCluster(),
                             input.getTraitSet(), traversalScan, rowType);
@@ -156,7 +141,6 @@ public class SingleQueryExecutor {
             final Enumerable<Object> enumerable = bindable.bind(null);
             rowResults = enumerable.toList();
         } else {
-            // if it is a convertable node, traversal gets the values
             final List<Map<Object, Object>> results = traversal.valueMap().toList();
             final List<Object> rows = new ArrayList<>();
             final List<String> fieldNames = node.getRowType().getFieldNames();
@@ -174,7 +158,6 @@ public class SingleQueryExecutor {
                 }
                 rows.add(row);
             }
-            // use the traversal to get results
             rowResults = rows;
         }
         return rowResults;
@@ -226,7 +209,6 @@ public class SingleQueryExecutor {
             return null;
         }
 
-        // go until we hit a converter to find the input
         RelNode input = node;
         RelNode parent = node;
         while (!((input = input.getInput(0)) instanceof GremlinToEnumerableConverter)) {
@@ -270,13 +252,7 @@ public class SingleQueryExecutor {
         executor.execute(pagination);
         executor.shutdown();
 
-        //        try {
-        //            executor.awaitTermination(20, TimeUnit.SECONDS);
-        //        } catch (InterruptedException e) {
-        //            e.printStackTrace();
-        //        }
         return sqlGremlinQueryResult;
-        //        return convertResult(rows, input, parent, rowType);
     }
 
     public SqlGremlinQueryResult handleEdge() {
@@ -284,7 +260,6 @@ public class SingleQueryExecutor {
             return null;
         }
 
-        // Go until we hit a converter to find the input.
         RelNode input = node;
         RelNode parent = node;
         while (!((input = input.getInput(0)) instanceof GremlinToEnumerableConverter)) {
@@ -316,22 +291,13 @@ public class SingleQueryExecutor {
         final Runnable pagination = new EdgePagination(input, parent);
         executor.execute(pagination);
         executor.shutdown();
-        //        try {
-        //            executor.awaitTermination(20, TimeUnit.SECONDS);
-        //        } catch (InterruptedException e) {
-        //            e.printStackTrace();
-        //        }
 
         return sqlGremlinQueryResult;
-        //return convertResult(rows, input, parent, rowType);
     }
 
-    //    SqlGremlinQueryResult convertResult(final List<Object> rows, final RelNode input, final RelNode parent,
-    //                                        final RelDataType rowType) {
-    //        return new SqlGremlinQueryResult(input.getCluster().getPlanner().getRoot().getRowType().getFieldNames(), table);
-    //    }
-
-    // converts input row results and insert them into sqlGremlinQueryResult
+    /**
+     * converts input row results and insert them into sqlGremlinQueryResult
+     */
     void convertAndInsertResult(final SqlGremlinQueryResult sqlGremlinQueryResult, final List<Object> rows,
                                 final RelNode input, final RelNode parent,
                                 final RelDataType rowType) {
@@ -367,17 +333,6 @@ public class SingleQueryExecutor {
         private final List<String> columns;
         private final List<String> columnTypes = new ArrayList<>();
         private final Object assertEmptyLock = new Object();
-        // private final List<List<Object>> rows = new ArrayList<>();
-        // blocking queue is shared between threads, here we use it so we can take elements out of it, while we put
-        // elements inside
-
-        // blocking queue is used when you have multiple threads put and take items from it, so do we want to use the queue
-        // in the way that set the queue size to page limit, then have launch a thread that continuously gets all results
-        // dumping them into the queue, then a full queue will block the thread from adding elements, then the results
-        // get dumped into a list to be returned? At that point the thread will continue to dump result
-
-        // or do we want an unbounded blocking queue, for which we will only block on empty, and then continuously put
-        // results into the queue, but have the thread monitor size of the result list and insert when we are not at capacity?
         private final BlockingQueue<List<Object>> blockingQueueRows = new LinkedBlockingQueue<>();
         private boolean isEmpty = false;
         private Thread currThread = null;
@@ -396,66 +351,36 @@ public class SingleQueryExecutor {
 
         public void assertIsEmpty() {
             synchronized (assertEmptyLock) {
-                // insert specific stop signal
-                // blockingQueueRows.add(null);
-                // interrupt here?
-                // Thread.currentThread().interrupt();
-                //
                 if (currThread != null && blockingQueueRows.size() == 0) {
-                    System.out.println("assertIsEmpty() INTERRUPT THREAD CALL");
                     currThread.interrupt();
                 }
                 isEmpty = true;
             }
         }
 
-        // add result here needs to be the converted rows
         public void addResults(final List<List<Object>> rows) {
             blockingQueueRows.addAll(rows);
         }
 
-        // get result blocks if the queue is empty, so we have to make sure that is empty doesn't change when it is
-        // waiting for the queue;
-        // is it possible for us to issue getResult --> result set not empty yet but empty queue --> starts waiting
-        // --> then the thread asserts it is empty --> but we are stuck waiting?
         public Object getResult() {
             try {
                 synchronized (assertEmptyLock) {
-                    System.out.println("getResult() INSIDE LOCK");
                     // pass current thread in, and interrupt in assertIsEmpty
                     this.currThread = Thread.currentThread();
                     if (isEmpty && blockingQueueRows.size() == 0) {
-                        System.out.println("getResult() IS EMPTY");
                         return null;
                     }
                 }
-                System.out.println("getResult() CURRENT SIZE: " + this.blockingQueueRows.size());
-                System.out.println("getResult() RETURN RESULT");
                 return this.blockingQueueRows.take();
             } catch (final InterruptedException ignored) {
-                System.out.println("getResult() INTERRUPTED: " + ignored);
                 return null;
             }
-        }
-
-        public int getPageSize() {
-            return pageSize;
-        }
-
-        // TODO: should this be something SqlGremlinQueryResult manage? this is done only because SqlGremlinQueryResult
-        //  is the only object being passed through the ResultSet constructor
-        public void setPageSize(int size) {
-            if (size <= 0) {
-                pageSize = DEFAULT_PAGE_SIZE;
-            }
-            pageSize = size;
         }
     }
 
     public class VertexPagination implements Runnable {
         private RelNode input;
         private RelNode parent;
-        private int cumulativeRes = 0;
 
         VertexPagination(final RelNode input, final RelNode parent) {
             this.input = input;
@@ -464,14 +389,10 @@ public class SingleQueryExecutor {
 
         @Override
         public void run() {
-            System.out.println("VERTEX PAGINATION START");
-            // Monitor size of sqlGremlinQueryResult list and insert on the fly.
             while (traversal.hasNext()) {
-                System.out.println("VERTEX PAGINATION BATCH START SIZE: " + cumulativeRes);
                 final RelDataType rowType = input.getRowType();
                 final List<String> fieldNames = rowType.getFieldNames();
                 final List<Map<String, Object>> results = (List<Map<String, Object>>) traversal.next(pageSize);
-                cumulativeRes += results.size();
                 final List<Object> rows = new ArrayList<>();
                 for (final Map<String, Object> map : results) {
                     final Map<Object, Object> mapResult = (Map<Object, Object>) map.get(MAP_KEY);
@@ -508,11 +429,9 @@ public class SingleQueryExecutor {
                     }
                     rows.add(row);
                 }
-                System.out.println("VERTEX PAGINATION BATCH DONE");
                 convertAndInsertResult(sqlGremlinQueryResult, rows, input, parent, rowType);
             }
-            // If we run out of traversal data (or hit our limit), stop inserting and signal to the result that it is done.
-            System.out.println("VERTEX PAGINATION DONE. FINAL SIZE: " + cumulativeRes);
+            // If we run out of traversal data (or hit our limit), stop and signal to the result that it is done.
             sqlGremlinQueryResult.assertIsEmpty();
         }
     }
@@ -528,10 +447,7 @@ public class SingleQueryExecutor {
 
         @Override
         public void run() {
-            System.out.println("EDGE PAGINATION START");
-            // Monitor size of sqlGremlinQueryResult list and insert on the fly.
             while (traversal.hasNext()) {
-                System.out.println("EDGE PAGINATION BATCH START");
                 final RelDataType rowType = input.getRowType();
                 final List<String> fieldNames = rowType.getFieldNames();
 
@@ -571,11 +487,9 @@ public class SingleQueryExecutor {
                     }
                     rows.add(row);
                 }
-                System.out.println("EDGE PAGINATION BATCH DONE");
                 convertAndInsertResult(sqlGremlinQueryResult, rows, input, parent, rowType);
             }
-            // If we run out of traversal data (or hit our limit), stop inserting and signal to the result that it is done.
-            System.out.println("EDGE PAGINATION DONE");
+            // If we run out of traversal data (or hit our limit), stop and signal to the result that it is done.
             sqlGremlinQueryResult.assertIsEmpty();
         }
     }
